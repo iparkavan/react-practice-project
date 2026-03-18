26f07dc0552339f685a2033cd42bc99ef8ac57bb
Park Avan <ipark.avan@gmail.com>
2026-03-18 15:46:56 +0530

    container with most water

--- src/features/interview-questions/ArrayAndStrings.js
+++ src/features/interview-questions/ArrayAndStrings.js
@@ -246,3 +246,98 @@ const mergeWithoutExtraSpace = (arr1, arr2) => {
 mergeWithoutExtraSpace(arr1, arr2);
 
 console.log(arr1, arr2);
+
+// • [10] 3Sum
+
+const array = [-1, 0, 1, 2, -1, -4];
+
+const threeSum = (arr) => {
+  const set = new Set();
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      let sum = arr[i] + arr[left] + arr[right];
+
+      if (sum === 0) {
+        set.add([arr[i], arr[left], arr[right]].toString());
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return [...set].map((item) => item.split(",").map(Number));
+};
+
+console.log(threeSum(array));
+
+// ----
+
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+
+  let result = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return result;
+}
+
+// • [11] Container With Most Water
+
+let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+const maxWater = (height) => {
+  let left = 0;
+  let right = height.length - 1;
+  let max = 0;
+
+  while (left < right) {
+    const h = Math.min(height[left], height[right]);
+    const width = right - left;
+    const area = h * width;
+
+    max = Math.max(max, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+};
+
+console.log(maxWater(height));
